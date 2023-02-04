a0cfb41d9270fd6a78a1562161b82311fb092b30
Yajnesh <starwolfyako@gmail.com>
2023-02-04 13:25:36 +0530

    added ggl forms

--- assets/js/func.js
+++ assets/js/func.js
@@ -11,3 +11,14 @@ function closeModal(id) {
     modal.style.display = "none";
     };
     
+function send_data(){
+    var form_data = document.getElementsByClassName("form-data"); //retrieve filled form data
+    var i;
+    var data = [];
+    for(i=0; i<form_data.length; i++){
+        data.push(form_data[i].value);
+    }
+    google.script.run.saveData(data); // send to google app script
+    document.getElementById("form").style.display = "none"; // make form invisible
+    document.getElementById("completion-msg").style.display = "block"; // Optional if you want to give a completion feedback!
+    }
